36be46a3d49465b721ed24d01aa1b1cd4c2edb90
Mark Peters <mapeters101@earthlink.net>
2025-03-25 15:34:30 -0400

    Work on lesson017 from 7:00 to 9:50.

--- lesson017.py
+++ lesson017.py
@@ -4,4 +4,12 @@ print(id(a))
 print(sys.getrefcount(a))
 print('-'*80)
 
+import ctypes
+def ref_count(address: int):
+    return ctypes.c_long.from_address(address).value
+
+print(ref_count(id(a)))
+print(ctypes.c_long.from_address(id(a)).value)
+print('-'*80)
+
 
