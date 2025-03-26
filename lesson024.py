af979a4490b816e59620ca8f74a07ac940eed08b
Mark Peters <mapeters101@earthlink.net>
2025-03-26 19:35:08 -0400

    Work on lesson024 from 10:20 to 11:00.

--- lesson024.py
+++ lesson024.py
@@ -14,4 +14,11 @@ print("a is b:", a is b)
 print("a == b:", a == b)
 print('-'*80)
 
+a = [1,2,3]
+b = [1,2,3]
+print(hex(id(a)))
+print(hex(id(b)))
+print("a is b:", a is b)
+print("a == b:", a == b)
+print('-'*80)
 
