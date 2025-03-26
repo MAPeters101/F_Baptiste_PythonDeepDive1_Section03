a2d73700074dafc4c3ada0086c036744174659f9
Mark Peters <mapeters101@earthlink.net>
2025-03-26 19:33:52 -0400

    Work on lesson024 to 10:20.

---
+++ lesson024.py
@@ -0,0 +1,17 @@
+a = 10
+b = 10
+print(hex(id(a)))
+print(hex(id(b)))
+print("a is b:", a is b)
+print("a == b:", a == b)
+print()
+
+a = 1000
+b = 1000
+print(hex(id(a)))
+print(hex(id(b)))
+print("a is b:", a is b)
+print("a == b:", a == b)
+print('-'*80)
+
+
