8ffa44bb929406c1ec904a9901ecfa827d48795d
Mark Peters <mapeters101@earthlink.net>
2025-03-26 18:40:18 -0400

    Work on lesson022 from 11:50 to 14:30.

--- lesson022.py
+++ lesson022.py
@@ -11,3 +11,18 @@ print('After function call: my_var # = {0}'.format(hex(id(my_var))))
 print(my_var)
 print('-'*80)
 
+def modify_list(lst):
+    print('Initial lst # = {0}'.format(hex(id(lst))))
+    lst.append(100)
+    print('Final lst # = {0}'.format(hex(id(lst))))
+
+my_list = [1,2,3]
+print('my_list # = {0}'.format(hex(id(my_list))))
+modify_list(my_list)
+print('After function call: my_list # = {0}'.format(hex(id(my_list))))
+print(my_list)
+print('-'*80)
+
+
+
+
