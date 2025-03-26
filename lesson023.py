1ee9cfb5dcee6a6c296d4bb6bcc268ef9189e44e
Mark Peters <mapeters101@earthlink.net>
2025-03-26 19:23:25 -0400

    Work on lesson023 from 6:40 to 8:20.

--- lesson023.py
+++ lesson023.py
@@ -15,5 +15,21 @@ print(hex(id(a)))
 print(hex(id(b)))
 print('-'*80)
 
+a = [1,2,3]
+b = a
+print(hex(id(a)))
+print(hex(id(b)))
+print()
+
+b.append(100)
+print(hex(id(a)))
+print(hex(id(b)))
+print(a)
+print(b)
+print()
+print('-'*80)
+
+
+
 
 
