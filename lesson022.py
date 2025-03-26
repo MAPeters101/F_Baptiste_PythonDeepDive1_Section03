b8dde3e48c4ec12a81ec4f89d2be1f5d147f522a
Mark Peters <mapeters101@earthlink.net>
2025-03-26 18:24:41 -0400

    Work on lesson022 to 11:50.

---
+++ lesson022.py
@@ -0,0 +1,13 @@
+
+def process(s):
+    print('Initial s # = {0}'.format(hex(id(s))))
+    s = s + ' world'
+    print('Final s # = {0}'.format(hex(id(s))))
+
+my_var = 'hello'
+print('my_var # = {0}'.format(hex(id(my_var))))
+process(my_var)
+print('After function call: my_var # = {0}'.format(hex(id(my_var))))
+print(my_var)
+
+print('-'*80)
