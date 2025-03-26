9e12980d75bce609cf69a5a4ab78f5a8fe02c57d
Mark Peters <mapeters101@earthlink.net>
2025-03-26 18:38:10 -0400

    Work on lesson022 to 11:50.

--- lesson022.py
+++ lesson022.py
@@ -11,29 +11,3 @@ print('After function call: my_var # = {0}'.format(hex(id(my_var))))
 print(my_var)
 print('-'*80)
 
-def modify_list(lst):
-    print('Initial lst # = {0}'.format(hex(id(lst))))
-    lst.append(100)
-    print('Final lst # = {0}'.format(hex(id(lst))))
-
-my_list = [1,2,3]
-print('my_list # = {0}'.format(hex(id(my_list))))
-modify_list(my_list)
-print('After function call: my_list # = {0}'.format(hex(id(my_list))))
-print(my_list)
-print('-'*80)
-
-def modify_tuple(t):
-    print('Initial t # = {0}'.format(hex(id(t))))
-    t[0].append(100)
-    print('Final lst # = {0}'.format(hex(id(t))))
-
-my_tuple = ([1,2], 'a')
-print('my_tuple # = {0}'.format(hex(id(my_tuple))))
-modify_tuple(my_tuple)
-print('After function call: my_tuple # = {0}'.format(hex(id(my_tuple))))
-print(my_tuple)
-
-
-
-
