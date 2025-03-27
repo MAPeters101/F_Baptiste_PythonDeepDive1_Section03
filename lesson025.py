6d11fefd4cce982498ceca5f8f7e6703aa3b7094
Mark Peters <mapeters101@earthlink.net>
2025-03-26 20:08:06 -0400

    Work on lesson025 from 9:40 to 11:40.

--- lesson025.py
+++ lesson025.py
@@ -30,3 +30,24 @@ print(square(2))
 print(f(2))
 print('-'*80)
 
+def cube(a):
+    return a ** c
+
+def select_function(fn_id):
+    if fn_id == 1:
+        return square
+    else:
+        return cube
+
+f = select_function(1)
+print(f is square)
+print(f(2))
+print()
+
+f = select_function(2)
+print(f is cube)
+print(f(2))
+print()
+
+
+
