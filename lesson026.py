16698c1cee556fc97ecc1dd48348b703014e27a9
Mark Peters <mapeters101@earthlink.net>
2025-03-26 20:23:18 -0400

    Work on lesson026 to 7:20.

---
+++ lesson026.py
@@ -0,0 +1,31 @@
+a = 10
+b = 10
+print(hex(id(a)))
+print(hex(id(b)))
+print(a is b)
+print()
+
+a = -5
+b = -5
+print(hex(id(a)))
+print(hex(id(b)))
+print(a is b)
+print()
+
+a = 256
+b = 256
+print(hex(id(a)))
+print(hex(id(b)))
+print(a is b)
+print()
+
+a = 257
+b = 257
+print(hex(id(a)))
+print(hex(id(b)))
+print(a is b)
+print('-'*80)
+
+
+
+
